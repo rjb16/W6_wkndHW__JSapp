52f45026079d38e24b04aaf4bc07fc586229efc4
Ruben Botha <rubenjohannesbotha@gmail.com>
2020-07-25 19:03:53 +0100

    Added submit button

---
+++ js/app.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript loaded');
+  
+    const form = document.querySelector('#new-item-form');
+    form.addEventListener('submit', handleFormSubmit); //listener for the form submit
+  });
+  
+  // this 
+  const handleFormSubmit = function(event) {
+    event.preventDefault(); //stops page refresh
+  
+    const hallOfFame = document.querySelector('#hall-of-fame'); 
+    const newItem = document.createElement('li'); //creates new li element 
+    newItem.textContent = `${event.target.first_name.value} ${event.target.last_name.value},
+    ${event.target.category.value}`;
+    
+    //gives it a class of item
+    newItem.classList.add('item'); 
+    
+  
+    hallOfFame.appendChild(newItem);
+  
+    //resets after each submission
+    document.getElementById('new-item-form').reset();
+  }
